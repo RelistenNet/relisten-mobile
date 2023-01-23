f441dc82f59f0ce11754840cc319f9971b89f9c2
Daniel Saewitz <switz213@gmail.com>
2023-01-23 01:52:49 -0500

    Add initial nativewind support

--- relisten/screens/home/Relisten.tsx
+++ relisten/screens/home/Relisten.tsx
@@ -1,10 +1,13 @@
 import React, { PropsWithChildren } from 'react';
+import { View as RNView } from 'react-native';
 import { Text, View } from 'react-native-ui-lib';
 
 export const RelistenScreen: React.FC<PropsWithChildren<{}>> = () => {
   return (
-    <View useSafeArea flex style={{ width: '100%' }}>
-      <Text>Home Screen</Text>
+    <View useSafeArea flex style={{ width: '100%' }} className="bg-orange-600">
+      <RNView className="bg-green-600 h-40 flex items-center justify-center">
+        <Text className="text-red-300 text-5xl font-semibold">Home Screen 2</Text>
+      </RNView>
     </View>
   );
 };
