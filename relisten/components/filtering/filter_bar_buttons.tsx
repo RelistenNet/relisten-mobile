6c88bb1b9543af812a4a02124763378bc7f98faf
Daniel Saewitz <git@saewitz.com>
2023-12-07 16:32:11 -0500

    Clean up filter button icons; Fix asc/desc sort inversion

--- relisten/components/filtering/filter_bar_buttons.tsx
+++ relisten/components/filtering/filter_bar_buttons.tsx
@@ -23,22 +23,24 @@ export const FilterBarButton = <T extends RelistenObject>({
 >) => {
   const filterIcon = driver({
     states: {
+      isFilterActive: !filter.sortDirection && filter.active,
+      isFilterInactive: !filter.sortDirection && !filter.active,
+      isSortFilterInactive: !filter.active,
       isNumericAscending: filter.isNumeric && filter.sortDirection === SortDirection.Ascending,
       isNumericDescending: filter.isNumeric && filter.sortDirection === SortDirection.Descending,
       isAlphabeticalAscending: filter.sortDirection === SortDirection.Ascending,
       isAlphabeticalDescending: filter.sortDirection === SortDirection.Descending,
-      isFilterActive: !filter.sortDirection && filter.active,
-      isFilterInactive: !filter.sortDirection && !filter.active,
+
       fallback: true,
     },
     derived: {
       icon: {
-        isNumericAscending: 'sort-numeric-ascending' as const,
-        isNumericDescending: 'sort-numeric-descending' as const,
-        isAlphabeticalAscending: 'sort-alphabetical-ascending' as const,
+        isNumericAscending: 'sort-ascending' as const,
+        isNumericDescending: 'sort-descending' as const,
+        isAlphabeticalAscending: 'sort-ascending' as const,
         isAlphabeticalDescending: 'sort-alphabetical-descending' as const,
-        isFilterActive: 'check-circle' as const,
-        isFilterInactive: 'check-circle-outline' as const,
+        isFilterActive: '' as const,
+        isFilterInactive: '' as const,
         fallback: 'sort-descending' as const,
       },
     },
@@ -47,14 +49,22 @@ export const FilterBarButton = <T extends RelistenObject>({
   return (
     <TouchableOpacity
       className={clsx(
-        'flex flex-row items-center rounded-xl p-1 px-2',
-        filter.active ? 'bg-relisten-blue-600' : 'bg-relisten-blue-800',
+        'flex flex-row items-center rounded-lg p-1 px-2',
+        filter.active ? 'bg-relisten-blue-600' : 'border border-relisten-blue-600/30',
         className
       )}
       {...props}
     >
-      <MaterialCommunityIcons name={filterIcon.icon} color={color || 'white'} size={size || 16} />
-      <View className="w-1" />
+      {filterIcon.icon && (
+        <>
+          <MaterialCommunityIcons
+            name={filterIcon.icon}
+            color={color || 'white'}
+            size={size || 16}
+          />
+          <View className="w-1" />
+        </>
+      )}
       <RelistenText className="text-base font-bold">
         {filter.title ? filter.title : children}
       </RelistenText>
