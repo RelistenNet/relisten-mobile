017acfff3c7a2e58947a601b84878cdcde87f3f2
Alec Gorge <alecgorge@gmail.com>
2024-12-23 11:31:34 -0600

    add logs to crashlytics

--- relisten/util/logging.ts
+++ relisten/util/logging.ts
@@ -1,11 +1,17 @@
-import { consoleTransport, logger } from 'react-native-logs';
+import { consoleTransport, crashlyticsTransport, logger } from 'react-native-logs';
 import { InteractionManager } from 'react-native';
+import crashlytics from '@react-native-firebase/crashlytics';
+
+const crashlyticsModule = crashlytics();
 
 export type LogLevels = 'debug' | 'info' | 'warn' | 'error';
 
 export const log = logger.createLogger<LogLevels>({
   // TODO: when we have entry, also log to a file so that we can attach it to crash reports
-  transport: consoleTransport,
+  transport: [consoleTransport, crashlyticsTransport],
+  transportOptions: {
+    CRASHLYTICS: crashlyticsModule,
+  },
   severity: __DEV__ ? 'debug' : 'info',
   async: true,
   asyncFunc: InteractionManager.runAfterInteractions,
