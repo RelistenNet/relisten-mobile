19ac6e63af469bdc5d8ed536e67d5c8aafef4782
Daniel Saewitz <git@saewitz.com>
2025-04-07 14:34:10 -0400

    Upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import main from '@switz/eslint-config/eslint.config.mjs';
 import react from '@switz/eslint-config/react.mjs';
-import typescript from '@switz/eslint-config/typescript.mjs';
 
-export default [...react, ...typescript, ...main];
+export default [...react, ...main];
