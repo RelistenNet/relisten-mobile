795f00d8ab5f88a203f8ae99e0aff7bd24569e61
Daniel Saewitz <990166+switz@users.noreply.github.com>
2026-02-19 18:32:45 -0500

    Add clearer warning when offline mode is enabled (#131)

--- relisten/components/row_with_action.tsx
+++ relisten/components/row_with_action.tsx
@@ -1,23 +1,35 @@
-import { PropsWithChildren } from 'react';
-import { ViewProps } from 'react-native';
 import Flex from '@/relisten/components/flex';
 import { RelistenText } from '@/relisten/components/relisten_text';
 import { tw } from '@/relisten/util/tw';
+import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
+import { ViewProps } from 'react-native';
 
 export function RowWithAction({
   children,
   title,
   subtitle,
   className,
+  warnOnEnable,
   ...props
-}: { title: string; subtitle?: string } & PropsWithChildren<ViewProps>) {
+}: { title: string; subtitle?: string; warnOnEnable?: boolean } & PropsWithChildren<ViewProps>) {
   return (
-    <Flex className={tw('w-full items-center justify-between', className)} {...props}>
-      <Flex column className="flex-shrink gap-1 pr-2">
-        {title && <RelistenText className="font-semibold">{title}</RelistenText>}
-        {subtitle && <RelistenText className="text-sm text-gray-400">{subtitle}</RelistenText>}
+    <Flex column>
+      <Flex className={tw('w-full items-center justify-between', className)} {...props}>
+        <Flex column className="flex-shrink gap-1 pr-2">
+          {title && <RelistenText className="font-semibold">{title}</RelistenText>}
+          {subtitle && (
+            <RelistenText
+              className={clsx('text-sm text-gray-400', {
+                ['text-amber-500 italic font-semibold']: !!warnOnEnable,
+              })}
+            >
+              {subtitle}
+            </RelistenText>
+          )}
+        </Flex>
+        {children}
       </Flex>
-      {children}
     </Flex>
   );
 }
